059b49049059667fa7652571594ad94503790fa5
csomi <csomos.adam.benedek@gmail.com>
2023-06-17 21:41:34 +0200

    stable not a game

--- public/particle.js
+++ public/particle.js
@@ -8,27 +8,44 @@ canvas.height = window.innerHeight;
 
 // Define the Particle class
 class Particle {
-  constructor(x, y, velocityX, velocityY, color, size) {
+  constructor(x, y, velocityX, velocityY, color, size, lifespan) {
     this.x = x;
     this.y = y;
     this.velocityX = velocityX;
     this.velocityY = velocityY;
     this.color = color;
     this.size = size;
+    this.lifespan = lifespan;
+    this.opacity = 1;
   }
 
   // Update the particle's position and properties
   update() {
     this.x += this.velocityX;
     this.y += this.velocityY;
+    this.lifespan--;
+
+    // Modify particle velocity
+    this.velocityX *= velocityModifier;
+    this.velocityY *= velocityModifier;
+
+    // Modify particle opacity based on lifespan
+    this.opacity = this.lifespan / initialLifespan;
+
     // Additional logic for particle behavior, e.g., bounce off walls
+
+    // Delete particle if lifespan is zero or negative
+    if (this.lifespan <= 0) {
+      const particleIndex = particles.indexOf(this);
+      particles.splice(particleIndex, 1);
+    }
   }
 
   // Render the particle on the canvas
   render() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fillStyle = this.color;
+    ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`;
     ctx.fill();
     ctx.closePath();
   }
@@ -37,22 +54,24 @@ class Particle {
 // Create an array to store the particles
 const particles = [];
 
+// Parameters for particle system
+const initialLifespan = 1000; // Initial lifespan of particles
+const deletionThreshold = 10; // Lifespan threshold for particle deletion
+const velocityModifier = 0.99; // Velocity modifier for particles
+
 // Generate particles and add them to the array
 function generateParticles() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     const x = canvas.width / 2;
     const y = canvas.height / 2;
     const velocityX = (Math.random() - 0.5) * 5;
     const velocityY = (Math.random() - 0.5) * 5;
-    const color = '#ffffff';
+    const color = '255, 255, 255';
     const size = Math.random() * 5 + 1;
 
-    particles.push(new Particle(x, y, velocityX, velocityY, color, size));
+    particles.push(new Particle(x, y, velocityX, velocityY, color, size, initialLifespan));
   }
 }
-
-
-setInterval(generateParticles, 100);
 
 // Update and render the particles
 function updateParticles() {
@@ -62,6 +81,14 @@ function updateParticles() {
     particles[i].update();
     particles[i].render();
   }
+
+  // Delete particles with low lifespan
+  particles.forEach((particle) => {
+    if (particle.lifespan <= deletionThreshold) {
+      const particleIndex = particles.indexOf(particle);
+      particles.splice(particleIndex, 1);
+    }
+  });
 
   requestAnimationFrame(updateParticles);
 }
